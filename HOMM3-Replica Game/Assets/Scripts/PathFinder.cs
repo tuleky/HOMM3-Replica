6c3c065b80d4188e561de133ec54ed07b117f882
Yusuf Enes Tulek <enes_tulek@live.com>
2021-03-28 18:08:06 +0300

    Shortened GetNeighbours algorithm

--- HOMM3-Replica Game/Assets/Scripts/PathFinder.cs
+++ HOMM3-Replica Game/Assets/Scripts/PathFinder.cs
@@ -4,31 +4,45 @@ using UnityEngine;
 
 public static class PathFinder
 {
+	// west and east are straightforward (-1,0) and (+1,0), diagonals are north west (-1, +1), north east (+1, +1), south west (-1, -1), south east (+1, -1)
+	static readonly Vector2Int[] DIRECTIONS =
+	{
+		new Vector2Int(-1, 0),
+		new Vector2Int(-1, 0),
+		new Vector2Int(1, 0),
+		new Vector2Int(-1, 1),
+		new Vector2Int(1, 1),
+		new Vector2Int(-1, -1), 
+		new Vector2Int(1, -1)
+	};
+
+	static Dictionary<GridPiece, GridPiece> cameFromDictionary = new Dictionary<GridPiece, GridPiece>();
+	
 	public static List<GridPiece> GetAvailablePaths(UnitMono chosenUnitMono)
 	{
 		// try to map a moveable cells based on chosen unit and grids
 		
-		int moveDistance = chosenUnitMono.MoveSpeed;
-		
 		// here we will do pathfinding solution for finding all possible moveable paths
-		Vector2Int startingPointIndex = chosenUnitMono.GridIndex;
-		GridPiece startingPointGridPiece = GridPiece.GetGridPieceByIndex(startingPointIndex);
-
+		GridPiece startingPointGridPiece = GridPiece.GetGridPieceByIndex(chosenUnitMono.GridIndex);
+		
+		
 		List<GridPiece> availableGridPieces = new List<GridPiece>();
 
-		List<GridPiece> firstIteration = new List<GridPiece>();
-		firstIteration.Add(startingPointGridPiece);
-		
-		for (int i = 0; i < moveDistance; i++)
+		List<GridPiece> tempIteration = new List<GridPiece>
+		{
+			startingPointGridPiece
+		};
+
+		for (int i = 0; i < chosenUnitMono.MoveSpeed; i++)
 		{
 			// dynamically change startingPointGridPiece so every iteration calculates for new points
 
 			List<GridPiece> newNeighbors = new List<GridPiece>();
-			foreach (GridPiece gridPiece in firstIteration)
+			foreach (GridPiece gridPiece in tempIteration)
 			{
 				newNeighbors.AddRange(GetNeighbors(gridPiece));
 			}
-			firstIteration = newNeighbors;
+			tempIteration = newNeighbors;
 
 			foreach (GridPiece gridPiece in newNeighbors)
 			{
@@ -45,21 +59,10 @@ public static class PathFinder
 	static List<GridPiece> GetNeighbors(GridPiece gridPiece)
 	{
 		List<GridPiece> result = new List<GridPiece>();
-
-		List<Vector2Int> temp = new List<Vector2Int>
+		
+		foreach (Vector2Int vector2Int in DIRECTIONS)
 		{
-			gridPiece.GridIndex + new Vector2Int(-1, 0),
-			gridPiece.GridIndex + new Vector2Int(1, 0),
-			gridPiece.GridIndex + new Vector2Int(-1, 1),
-			gridPiece.GridIndex + new Vector2Int(1, 1),
-			gridPiece.GridIndex + new Vector2Int(-1, -1),
-			gridPiece.GridIndex + new Vector2Int(1, -1)
-		};
-		// west and east are straightforward (-1,0) and (+1,0), diagonals are north west (-1, +1), north east (+1, +1), south west (-1, -1), south east (+1, -1)
-
-		foreach (Vector2Int vector2Int in temp)
-		{
-			GridPiece foundGridPieceByIndex = GridPiece.GetGridPieceByIndex(vector2Int);
+			GridPiece foundGridPieceByIndex = GridPiece.GetGridPieceByIndex(gridPiece.GridIndex + vector2Int);
 			if (foundGridPieceByIndex != null && !foundGridPieceByIndex.HasUnit())
 			{
 				result.Add(foundGridPieceByIndex);
@@ -68,4 +71,10 @@ public static class PathFinder
 
 		return result;
 	}
+
+	static void TraceBackFromGoalToStart()
+	{
+		
+	}
+	
 }
